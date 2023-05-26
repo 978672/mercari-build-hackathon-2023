5e83e51686023e27a90bb08614f1dc174d513d85
kae <maple.mr08@gmail.com>
2023-05-26 18:36:55 +0900

    update header

--- frontend/simple-mercari-web/src/components/Header/Header.tsx
+++ frontend/simple-mercari-web/src/components/Header/Header.tsx
@@ -3,6 +3,13 @@ import { useNavigate } from "react-router-dom";
 import "./Header.css";
 import logo from '../../logo.png';
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 /* font awesome */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
@@ -19,27 +26,53 @@ export const Header: React.FC = () => {
 
   return (
     <>
+    <head>
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+  crossOrigin="anonymous"
+/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"></link>
+    </head>
       <header>
-        <div className="headerWrapper">
-          <div className="LogoContainer" onClick={() => navigate("/")}>
-            <img className="logo" src={logo} alt="Logo" />
-          </div>
-          <div className="searchContainer">
-            {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
-            {/* adding search icon  */}
-            <input type="text" id="MerTextInput" className="SearchBar"  placeholder={`何をお探しですか ${<FontAwesomeIcon icon={faMagnifyingGlass} />}`}/>
-          </div>
-          <div className="navContainer">
-            <span className="navButton" onClick={() => navigate("/")}>Home</span>
-            <span className="navButton" onClick={() => navigate("/sell")}>Listing</span>
-            <span className="navButton" onClick={() => navigate(`/user/${cookies.userID}`)}>MyPage</span>
-          </div>
-          <div className="LogoutButtonContainer">
-            <button onClick={onLogout} id="MerButton">
-              Logout
-            </button>
-          </div>
-        </div>
+        
+        {/* <div className="headerWtapper"> */}
+          {/* <i className="bi bi-house-door-fill"></i> */}
+          <Navbar bg="light" expand="lg">
+            <Container fluid>
+              <Navbar.Brand href="#"><img className="logo" src={logo} alt="Logo" /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="何をお探しですか"
+                    className="me-2"
+                    aria-label="Search"
+                    id="MerTextInput"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+                </Nav>
+                
+                  {/* <Nav.Link href="#" disabled>
+                    logout
+                  </Nav.Link> */}
+                  <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
+                  <Nav.Link onClick={() => navigate('/sell')}>Listening</Nav.Link>
+                  <Nav.Link onClick={() => navigate(`/user/${cookies.userID}`)}>Mypage</Nav.Link>
+                  <Button variant="danger" onClick={onLogout}>logout</Button>{' '}
+                  {/* <Button variant="danger" onClick={onLogout} id="MerButton">logout</Button>{' '}  */}
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        {/* </div> */}
       </header>
     </>
   );
